89e4e51fc5b0501cde8c62f05d4a6c003995c19b
Shenixx <113193025+Shenixx@users.noreply.github.com>
2024-02-06 09:57:27 -0500

    sadasd

---
+++ vite-project/src/components/DestCard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>{{ Destination.name }}</h2>
+        <img :scr="Destination.img" alt=""/>
+    </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+    const props = defineProps({
+        Destination: Object,
+    });
+//clicker logic
+const clicked = ref(0);
+
+function increment(){
+    clicked.value++;
+}
+</script>
+
+
+<style scoped>
+</style>
