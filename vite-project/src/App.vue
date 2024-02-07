68f4fff83249e8212ef6857cf3846a060d6e1a1e
Shenixx <113193025+Shenixx@users.noreply.github.com>
2024-02-07 09:37:43 -0500

    wew

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,21 +1,21 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
 </script>
 
 <template>
   <header>
-    <img
+    <!-- <img
       alt="Vue logo"
       class="logo"
       src="@/assets/logo.svg"
       width="125"
       height="125"
-    />
+    /> -->
 
     <div class="wrapper">
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <!-- <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink> -->
       </nav>
     </div>
   </header>
