6732f42d1e05feff66d33ac3c22f2a67cb4b95fa
Shenixx <113193025+Shenixx@users.noreply.github.com>
2024-03-02 22:04:23 -0500

    <isjdf8i9sdfjai9jad9s8efj98das

---
+++ vite-project/src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <cartitem v-for="candy in candy" :key="candy.name" :candy="candy"/>
+    <h3>{{total}}</h3>
+  </div>
+</template>
+
+
+<script setup>
+import { total } from '@/stores/counter';
+import { candy } from '@/stores/counter';
+</script>
+
+
+<style scoped>
+
+
+</style>
--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -1,13 +1,10 @@
 <script setup>
-import shopitem from '@/components/shopitem.vue';
 import { candy } from '@/stores/counter';
 </script>
 
 
 <template>
   <main>
-
-
     <shopitem v-for="candy in candy" :key="candy.name" :candy="candy"/>
   </main>
 </template>
